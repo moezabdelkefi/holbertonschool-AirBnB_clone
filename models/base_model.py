2feef024d18ca5ba60f0e9a0c13aaa97a5ebc4c5
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-21 15:32:59 +0100

    BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -2,30 +2,24 @@
 """define a class BaseModel"""
 
 import uuid
-from datetime import datetime
+import datetime
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        """Initialize BaseModel instance."""
+    def __init__(self):
         self.id = str(uuid.uuid4())
-        # returns the current date and time
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        """Return string representation of BaseModel instance."""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute with current datetime."""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """return dictionary"""
-        # Isoformat()function is used to return a string of date, time ..
-        dict_copy = self.__dict__.copy()
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
-        dict_copy['class'] = type(self).__name__
-        return dict_copy
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
