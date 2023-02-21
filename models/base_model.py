5cd9a444a3e6d23e1ed9a94a0c9940a70afc43e7
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-21 23:19:01 +0100

    class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,17 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
-        return "[{}] ({}) {}".format\
-            (self.__class__.__name__, self.id, self.__dict__)
+        """Return string representation of BaseModel instance."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Update updated_at attribute with current datetime."""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """return dictionary"""
+        # Isoformat()function is used to return a string of date, time ..
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
