a269fe63d9b2eaa5e67e0b469ee9baf6fcb7f276
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-23 14:07:14 +0100

    contains the entry point of the command interpreter

--- console.py
+++ console.py
@@ -9,11 +9,11 @@ class HBNBCommand(cmd.Cmd):
     """Command Interpreter"""
     prompt = '(hbnb) '
 
-    def do_quit(self):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self):
+    def do_EOF(self, arg):
         """EOF command to exit the program"""
         return True
 
