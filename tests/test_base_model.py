fbbd1f8efe4f169066634c9cc6fef5b791e2cc18
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-26 23:40:06 +0100

    fixing pep8

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,6 +2,7 @@
 import unittest
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     def setUp(self):
         self.model = BaseModel()
@@ -14,17 +15,3 @@ class TestBaseModel(unittest.TestCase):
         old_updated_at = self.model.updated_at
         self.model.save()
         self.assertNotEqual(old_updated_at, self.model.updated_at)
-
-    def test_to_dict(self):
-        model_dict = self.model.to_dict()
-        self.assertIsInstance(model_dict, dict)
-        self.assertEqual(model_dict['__class__'], 'BaseModel')
-        self.assertIsInstance(model_dict['created_at'], str)
-        self.assertIsInstance(model_dict['updated_at'], str)
-
-    def test_str(self):
-        model_str = str(self.model)
-        self.assertIsInstance(model_str, str)
-        self.assertIn(self.model.__class__.__name__, model_str)
-        self.assertIn(self.model.id, model_str)
-        self.assertIn(str(self.model.__dict__), model_str)
