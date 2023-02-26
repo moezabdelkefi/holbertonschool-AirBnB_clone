398e54aea980b82d0ce76789fde6ccfc650ed96d
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-26 21:53:32 +0100

    a class User that inherits from BaseModel

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+User module
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes User instance
+        """
+        super().__init__(*args, **kwargs)
