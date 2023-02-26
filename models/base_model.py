288130a98d9c535dc060c038bdfde2a1cc93a8e5
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-26 14:17:56 +0100

    fixing dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """define a class BaseModel"""
-
+import models
 import uuid
 import datetime
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
 
         if kwargs:
             for key, value in kwargs.items():
@@ -17,10 +14,19 @@ class BaseModel:
                     # Convert datetime string to datetime object
                     # sets the value of an attribute on an object.
                     # It takes three arguments name, value,object
+                    # strptime parse a string representing a datetime into
+                    # a datetime.datetime object
+                    #  used to convert datetime strings passed in as arguments
+                    # to the __init__ method (if any) into datetime.
+                    # datetime objects.
                     setattr(self, key, datetime.datetime.strptime(
                         value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Return string representation of BaseModel instance."""
@@ -30,6 +36,7 @@ class BaseModel:
     def save(self):
         """Update updated_at attribute with current datetime."""
         self.updated_at = datetime.datetime.now()
+        models.storage.save
 
     def to_dict(self):
         """return dictionary"""
