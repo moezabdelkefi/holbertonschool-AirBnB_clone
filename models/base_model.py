2294954f6a4bcf1092e09a6d39c82d424403eff4
moezabdelkef <moezabdelkefi17@gmail.com>
2023-02-26 23:39:40 +0100

    fixing pep8

--- models/base_model.py
+++ models/base_model.py
@@ -19,13 +19,16 @@ class BaseModel:
                     #  used to convert datetime strings passed in as arguments
                     # to the __init__ method (if any) into datetime.
                     # datetime objects.
-                    value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
             create = kwargs['created_at']
             update = kwargs['updated_at']
-            self.created_at = datetime.datetime.strptime(create, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.datetime.strptime(update, '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.datetime.strptime(
+                create, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.datetime.strptime(
+                update, '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.datetime.now()
